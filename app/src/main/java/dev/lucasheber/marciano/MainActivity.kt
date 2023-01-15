63d5e16ca915356f31fb8b02cf5c146443386e6c
lucas <lucas.heber07@gmailcom>
2023-01-15 17:03:41 -0300

    refactor if out fit

--- app/src/main/java/dev/lucasheber/marciano/MainActivity.kt
+++ app/src/main/java/dev/lucasheber/marciano/MainActivity.kt
@@ -2,8 +2,6 @@ package dev.lucasheber.marciano
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -22,9 +20,9 @@ class MainActivity : AppCompatActivity() {
         btnEnviar = findViewById<Button>(R.id.btnEnviar)
 
         inputText.requestFocus()
-        btnEnviar.setOnClickListener(View.OnClickListener {
+        btnEnviar.setOnClickListener {
             analisaMensagem(inputText.text.toString())
-        })
+        }
     }
 
     private fun analisaMensagem(message: String) {
@@ -32,12 +30,12 @@ class MainActivity : AppCompatActivity() {
         val list = listOf<String>("subtraia", "some", "multiplique", "divida")
         val intent: Intent
 
-        if (list.contains(message)) {
-            intent = Intent(this, PegaValoresActivity::class.java).apply {
+        intent = if (list.contains(message)) {
+            Intent(this, PegaValoresActivity::class.java).apply {
                 putExtra("message", message)
             }
         } else {
-            intent = Intent(this, AnswerAcktivity::class.java).apply {
+            Intent(this, AnswerAcktivity::class.java).apply {
                 putExtra("answer", marciano.responda(message))
             }
         }
